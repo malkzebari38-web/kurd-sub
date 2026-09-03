345f92bb673f572de67b81a1d50cb248d8427827
malkzebari38-web <malkzebari38@gmail.com>
2026-09-03 22:30:06 +0300

    Create subtitle_model.dart

---
+++ lib/subtitle_model.dart
@@ -0,0 +1,13 @@
+class SubtitleItem {
+  int index;
+  Duration start;
+  Duration end;
+  String text;
+
+  SubtitleItem({
+    required this.index,
+    required this.start,
+    required this.end,
+    required this.text,
+  });
+}
